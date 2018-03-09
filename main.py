9b5ac0c849b7b7237f4b19e737fb951c22fff050
German <german.nikolishin@gmail.com>
2018-03-09 13:53:55 +0300

    fix

--- main.py
+++ main.py
@@ -19,25 +19,22 @@ def echo(message):
     bot.reply_to(message, 'Hello, world')
 
 
-if 'HEROKU' in list(os.environ.keys()):
-    logger = telebot.logger
-    telebot.logger.setLevel(logging.INFO)
+logger = telebot.logger
+telebot.logger.setLevel(logging.INFO)
 
 
-    @server.route('/bot', methods=['POST'])
-    def get_message():
-        bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-        return "!", 200
+@server.route('/bot', methods=['POST'])
+def get_message():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
 
 
-    @server.route('/')
-    def webhook():
-        bot.remove_webhook()
-        bot.set_webhook(url='https://brand-bot.herokuapp.com/' + TOKEN)
-        return '!', 200
-
-    if __name__ == '__main__':
-        server.run(host='0.0.0.0', port=int(os.environ.get('PORT', 80)))
-else:
+@server.route('/')
+def webhook():
     bot.remove_webhook()
-    bot.polling(True)
+    bot.set_webhook(url='https://brand-bot.herokuapp.com/' + TOKEN)
+    return '!', 200
+
+
+if __name__ == '__main__':
+    server.run(host='0.0.0.0', port=int(os.environ.get('PORT', 80)))
