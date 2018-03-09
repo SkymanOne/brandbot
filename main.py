9e4d6c764003138e09d7f5fea7a6cef381829587
German <german.nikolishin@gmail.com>
2018-03-09 13:50:42 +0300

    change port

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ if 'HEROKU' in list(os.environ.keys()):
         return '!', 200
 
     if __name__ == '__main__':
-        server.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+        server.run(host='0.0.0.0', port=int(os.environ.get('PORT', 80)))
 else:
     bot.remove_webhook()
     bot.polling(True)
