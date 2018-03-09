46478cc72d27590b407d18425c716070453693cb
German <german.nikolishin@gmail.com>
2018-03-09 22:20:18 +0300

    change for pooling and webhook

--- main.py
+++ main.py
@@ -22,19 +22,30 @@ def echo(message):
 logger = telebot.logger
 telebot.logger.setLevel(logging.INFO)
 
+if 'HEROKU' in list(os.environ.keys()):
+    @server.route('/bot', methods=['POST'])
+    def get_message():
+        bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+        return "!", 200
 
-@server.route('/bot', methods=['POST'])
-def get_message():
-    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-    return "!", 200
 
+    @server.route('/')
+    def webhook():
+        bot.remove_webhook()
+        bot.set_webhook(url='https://brand-bot.herokuapp.com/' + 'bot')
+        return '!', 200
 
-@server.route('/')
-def webhook():
-    bot.remove_webhook()
-    bot.set_webhook(url='https://brand-bot.herokuapp.com/' + 'bot')
-    return '!', 200
+    @server.route('/')
+    def webhook():
+        bot.remove_webhook()
+        bot.set_webhook(url='https://brand-bot.herokuapp.com/' + TOKEN)
+        return '!', 200
 
 
 if __name__ == '__main__':
     server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 80)))
+    if __name__ == '__main__':
+        server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 80)))
+else:
+    bot.remove_webhook()
+    bot.polling(True)
