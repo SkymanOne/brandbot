ffdf656578afb2f488c15e8bb1ecebece9f473cc
German <german.nikolishin@gmail.com>
2018-03-16 20:05:52 +0300

    post update

--- main.py
+++ main.py
@@ -453,7 +453,7 @@ if 'HEROKU' in list(os.environ.keys()):
 
 
     if __name__ == '__main__':
-        server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 80)))
+        server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
 else:
     bot.remove_webhook()
     bot.polling(True)
